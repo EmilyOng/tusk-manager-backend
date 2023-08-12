3fd482a084e06d7339218a6dfd1f911e7aaf6ebd
Emily Ong <emilyohq11@gmail.com>
2023-08-12 22:06:21 +0800

    (bug) fix associations in seed data

--- utils/seed/seed.go
+++ utils/seed/seed.go
@@ -1,79 +1,107 @@
 package models
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/EmilyOng/cvwo/backend/db"
 	"github.com/EmilyOng/cvwo/backend/models"
-	commonUtils "github.com/EmilyOng/cvwo/backend/utils/common"
 )
 
 // Generates sample seed data
 func SeedData(user *models.User) (err error) {
-	colors := commonUtils.GetDefaultColors()
-
-	var tags []*models.Tag
-	var tasks []*models.Task
-	var states []*models.State
-
-	// Create sample states for the current board
-	for i, state := range commonUtils.GetDefaultStates() {
-		states = append(states, &models.State{
-			Name:            state,
-			CurrentPosition: i,
-		})
+	// Create the board
+	board := models.Board{
+		Name:  "My first board",
+		Color: models.Cyan,
+		Members: []*models.Member{{
+			Role:   models.Owner,
+			UserID: &user.ID,
+		}},
 	}
 
-	res := db.DB.Create(&states)
-	if err = res.Error; err != nil {
+	result := db.DB.Create(&board)
+	if result.Error != nil {
+		return
+	}
+
+	// Create sample states for the current board
+	statesMap := map[string]*models.State{
+		"To Do": {
+			Name:            "To Do",
+			CurrentPosition: 0,
+			BoardID:         &board.ID,
+		},
+		"In Progress": {
+			Name:            "In Progress",
+			CurrentPosition: 1,
+			BoardID:         &board.ID,
+		},
+		"Completed": {
+			Name:            "Completed",
+			CurrentPosition: 2,
+			BoardID:         &board.ID,
+		},
+	}
+
+	var states []*models.State
+	for _, state := range statesMap {
+		states = append(states, state)
+	}
+
+	result = db.DB.Create(&states)
+	if result.Error != nil {
 		return
 	}
 
 	// Create sample tags for the current board
-	for i := 0; i < 3; i++ {
-		tags = append(tags, &models.Tag{
-			Name:  "Tag-" + fmt.Sprint(i),
-			Color: colors[i%6],
-		})
+	tagsMap := map[string]*models.Tag{
+		"Wellness": {Name: "Wellness", Color: models.Turquoise, BoardID: &board.ID},
+		"School":   {Name: "School", Color: models.Green, BoardID: &board.ID},
+		"Fun":      {Name: "Fun", Color: models.Yellow, BoardID: &board.ID},
 	}
 
-	res = db.DB.Create(&tags)
-	if err = res.Error; err != nil {
+	var tags []*models.Tag
+	for _, tag := range tagsMap {
+		tags = append(tags, tag)
+	}
+	result = db.DB.Create(&tags)
+	if result.Error != nil {
 		return
 	}
 
 	// Create sample tasks for the current board
-	for i := 0; i < 3; i++ {
-		t := time.Now().Add(24 * time.Hour)
-		tasks = append(tasks, &models.Task{
-			Name:        "Sample Task-" + fmt.Sprint(i),
+	dueDate := time.Now().Add(24 * time.Hour)
+	tasks := []*models.Task{
+		{
+			Name:        "Badminton Game @ Q",
 			Description: "The quick brown fox jumps over the lazy dog",
-			DueAt:       &t,
-			Tags:        tags,
+			DueAt:       &dueDate,
+			Tags:        []*models.Tag{tagsMap["Wellness"], tagsMap["Fun"]},
 			UserID:      &user.ID,
-			StateID:     &states[i%3].ID,
-		})
+			StateID:     &statesMap["To Do"].ID,
+			BoardID:     &board.ID,
+		},
+		{
+			Name:        "Algorithms Problem Set",
+			Description: "The quick brown fox jumps over the lazy dog",
+			DueAt:       &dueDate,
+			Tags:        []*models.Tag{tagsMap["School"], tagsMap["Fun"]},
+			UserID:      &user.ID,
+			StateID:     &statesMap["In Progress"].ID,
+			BoardID:     &board.ID,
+		},
+		{
+			Name:        "Coffee Brewing",
+			Description: "The quick brown fox jumps over the lazy dog",
+			DueAt:       &dueDate,
+			Tags:        []*models.Tag{tagsMap["Wellness"]},
+			UserID:      &user.ID,
+			StateID:     &statesMap["Completed"].ID,
+			BoardID:     &board.ID,
+		},
 	}
-	res = db.DB.Create(&tasks)
-	if err = res.Error; err != nil {
-		return
-	}
-
-	owner := models.Member{
-		Role:   models.Owner,
-		UserID: &user.ID,
-	}
-
-	res = db.DB.Create(&models.Board{
-		Name:    "My first board",
-		Color:   models.Cyan,
-		Tags:    tags,
-		Tasks:   tasks,
-		States:  states,
-		Members: []*models.Member{&owner},
-	})
-	if err = res.Error; err != nil {
+	result = db.DB.Create(&tasks)
+	if result.Error != nil {
 		return
 	}
 
